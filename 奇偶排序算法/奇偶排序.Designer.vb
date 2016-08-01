d91988c8ee70c3f993b54b5afddfd6ab2dce4015
Leon <xiaoyankeji@hotmail.com>
2016-08-01 14:33:01 +0800

    第一次提交

---
+++ 奇偶排序算法/奇偶排序.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class 奇偶排序
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(12, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(133, 232)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(151, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 30)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "生成随机数"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(149, 48)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 30)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "奇偶排序"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(236, 261)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "奇偶排序算法"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ 奇偶排序算法/奇偶排序.vb
@@ -0,0 +1,57 @@
+﻿Public Class 奇偶排序
+    Private Const NumberCount As Byte = 19
+    Private Const NumberMax As Byte = 200
+    Dim NumberList(0 To NumberCount - 1) As Byte
+
+    Private Sub 按钮_生成随机数(sender As Object, e As EventArgs) Handles Button1.Click
+        生成新的随机数组()
+        刷新显示列表()
+    End Sub
+
+    Private Sub 按钮_奇偶排序(sender As Object, e As EventArgs) Handles Button2.Click
+        奇偶排序()
+        刷新显示列表()
+    End Sub
+
+    Private Sub 奇偶排序()
+        Dim Sorted As Boolean = True
+
+        Do While Sorted
+            Sorted = False
+            '遍历奇数
+            For Index = 1 To NumberCount - 1 Step 2
+                If NumberList(Index) > NumberList(Index + 1) Then
+                    异或算法交换值(NumberList(Index), NumberList(Index + 1))
+                    Sorted = True
+                End If
+            Next
+            '遍历偶数
+            For Index = 0 To NumberCount - 2 Step 2
+                If NumberList(Index) > NumberList(Index + 1) Then
+                    异或算法交换值(NumberList(Index), NumberList(Index + 1))
+                    Sorted = True
+                End If
+            Next
+        Loop
+    End Sub
+
+    Private Sub 生成新的随机数组()
+        For Index As Byte = 0 To NumberCount - 1
+            NumberList(Index) = CByte(VBMath.Rnd() * NumberMax)
+        Next
+    End Sub
+
+    Private Sub 刷新显示列表()
+        ListBox1.Items.Clear()
+        For Index As Byte = 0 To NumberCount - 1
+            ListBox1.Items.Add(NumberList(Index))
+        Next
+    End Sub
+
+    Private Sub 异或算法交换值(ByRef Number1, ByRef Number2)
+        Number1 = Number1 Xor Number2
+        Number2 = Number2 Xor Number1
+        Number1 = Number1 Xor Number2
+    End Sub
+
+End Class
